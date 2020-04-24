5279b607fa7db59ab6986795569454dfe1c1271d
KolmogortsevaKI <38918076+KolmogortsevaKI@users.noreply.github.com>
2020-04-25 00:20:52 +0700

    s

---
+++ Plagin/BirdHouse.Library/HouseBuilder.cs
@@ -0,0 +1,131 @@
+﻿using Kompas6API5;
+using Kompas6Constants;
+using Kompas6Constants3D;
+using System;
+
+namespace BirdHouse.Library
+{
+    public class HouseBuilder
+    {
+
+        /// <summary>
+        /// Интерфейс компонента
+        /// </summary>
+        public ksPart iPart;
+
+        /// <summary>
+        /// Построить скворечник
+        /// </summary>
+        public void Build(ksPart iPart, KompasObject kompas, HouseParameters houseParameters)
+        {
+            this.iPart = iPart;
+            CreateMain(iPart, kompas, houseParameters);
+        }
+
+        /// <summary>
+        /// Функция выполняет построение 
+        /// </summary>
+        public void CreateMain(ksPart iPart, KompasObject kompas, HouseParameters houseParameters)
+        {
+            int thickness = houseParameters.Depth;
+            ksEntity iSketch;
+            ksSketchDefinition iDefinitionSketch;
+
+            CreateSketch(out iSketch, out iDefinitionSketch);
+            // Интерфейс для рисования = на скетче;
+            ksDocument2D iDocument2D = (ksDocument2D)iDefinitionSketch.BeginEdit();
+            // Построить прямоугольник (x1,y1, x2,y2, style)
+            ksRectangleParam part1 = (ksRectangleParam)kompas.GetParamStruct((short)StructType2DEnum.ko_RectangleParam);
+            part1.ang = 0; //Угол ?
+            part1.x = 10;
+            part1.y = 10;
+            part1.width = houseParameters.Width;
+            part1.height = houseParameters.Height; 
+            part1.style = 1; // При нуле не видно деталь.
+            iDocument2D.ksRectangle(part1);
+
+            // Закончить редактировать эскиз
+            iDefinitionSketch.EndEdit();
+
+            ExctrusionSketch(iPart, iSketch, thickness, true);
+        }
+
+
+        /// <summary>
+        /// Создать эскиз
+        /// </summary>
+        private void CreateSketch(out ksEntity iSketch, out ksSketchDefinition iDefinitionSketch, double offset = 0)
+        {
+            #region Создание смещенную плоскость -------------------------
+            // интерфейс смещенной плоскости
+            ksEntity iPlane = (ksEntity)iPart.NewEntity((short)Obj3dType.o3d_planeOffset);
+
+            // Получаем интрефейс настроек смещенной плоскости
+            ksPlaneOffsetDefinition iPlaneDefinition = (ksPlaneOffsetDefinition)iPlane.GetDefinition();
+
+            // Настройки : начальная позиция, направление смещения, расстояние от плоскости, принять все настройки (create)
+            iPlaneDefinition.SetPlane(iPart.GetDefaultEntity((short)Obj3dType.o3d_planeXOZ));
+            iPlaneDefinition.direction = true;
+            iPlaneDefinition.offset = offset;
+            iPlane.Create();
+            #endregion --------------------------------------------------
+
+            // Создаем обьект эскиза
+            iSketch = (ksEntity)iPart.NewEntity((short)Obj3dType.o3d_sketch);
+
+            // Получаем интерфейс настроек эскиза
+            iDefinitionSketch = iSketch.GetDefinition();
+
+            // Устанавливаем плоскость эскиза
+            iDefinitionSketch.SetPlane(iPlane);
+
+            // Теперь когда св-ва эскиза установлены можно его создать 
+            iSketch.Create();
+        }
+        // <summary>
+        /// Выдавливание по эскизу
+        /// </summary>
+        private void ExctrusionSketch(ksPart iPart, ksEntity iSketch, int dd, bool direction)
+        {
+            //Операция выдавливание
+            ksEntity entityExtr = (ksEntity)iPart.NewEntity((short)Obj3dType.o3d_bossExtrusion);
+
+            //Интерфейс операции выдавливания
+            ksBossExtrusionDefinition extrusionDef = (ksBossExtrusionDefinition)entityExtr.GetDefinition();
+
+            //Интерфейс структуры параметров выдавливания
+            ksExtrusionParam extrProp = (ksExtrusionParam)extrusionDef.ExtrusionParam();
+
+            //Эскиз операции выдавливания
+            extrusionDef.SetSketch(iSketch);
+
+            //Направление выдавливания
+            if (direction == false)
+            {
+                extrProp.direction = (short)Direction_Type.dtReverse;
+            }
+            else
+            {
+                extrProp.direction = (short)Direction_Type.dtNormal;
+            }
+
+            //Тип выдавливания
+            extrProp.typeNormal = (short)End_Type.etBlind;
+
+            //Глубина выдавливания
+            if (direction == false)
+            {
+                extrProp.depthReverse = dd;
+            }
+            else
+            {
+                extrProp.depthNormal = dd;
+            }
+            //Создание операции выдавливания
+            entityExtr.Create();
+        }
+
+
+    }
+
+}
---
+++ Plagin/BirdHouse.Library/HouseParameters.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BirdHouse.Library
+{
+    /// <summary>
+    /// Класс, содержащий параметры скворечника.
+    /// </summary>
+    public class HouseParameters
+	{
+        /// <summary>
+        /// Высота скворечника.
+        /// </summary>
+        private int _height;
+        /// <summary>
+        /// Высота расположения дупла.
+        /// </summary>
+        private int _hallowHeight;
+        /// <summary>
+        /// Длина жердочки.
+        /// </summary>
+        private int _lengthPerch;
+        /// <summary>
+        /// Диаметр жердочки.
+        /// </summary>
+        private int _diameterPerch;
+        /// <summary>
+        /// Глубина скворечника.
+        /// </summary>
+        private int _depth;
+        /// <summary>
+        /// Ширина скворечника.
+        /// </summary>
+        private int _width;
+        /// <summary>
+        /// Ширина крепежа.
+        /// </summary>
+        private int _widthFasteners;
+
+
+
+        /// <summary>
+        /// Создает экземпляр класса HouseParameters
+        /// </summary>
+        public HouseParameters(int height,
+            int hallowHeight,
+            int lengthPerch,
+            int diameterPerch,
+            int depth,
+            int width,
+            int widthFasteners)
+        {
+            _listError = new Dictionary<Parameters, string>();
+            _listError.Clear();
+            Height = height;
+            HallowHeight = hallowHeight;
+            LengthPerch = lengthPerch;
+            DiameterPerch = diameterPerch;
+            Depth = depth;
+            Width = width;
+            WidthFasteners = widthFasteners;
+        }
+        /// <summary>
+        /// Словарь, хранящий в себе список выявленных ошибок
+        /// при некорректном вводе параметеров втулки
+        /// </summary>
+        public Dictionary<Parameters, string> _listError;
+
+        /// <summary>
+        /// Возвращает и задаёт высоту скворечника от 250-500мм.
+        /// </summary>
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (value <= 500 && value >= 250)
+                    _height = value;
+                else
+                    throw new ArgumentException("Height has to be from 250 mm 500 mm.");
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт высоту расположения дупла.
+        /// </summary>
+        public int HallowHeight
+        {
+            get
+            {
+                return _hallowHeight;
+            }
+            set
+            {
+                if (value <= (_height-25) && value >25)
+                    _hallowHeight = value;
+                else
+                    throw new ArgumentException
+                        ("Hallow height depends on height.It has to be higher 25 mm and below on"
+                        +(_height- 25)+ " mm of top edge");
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт длину жердочки.
+        /// </summary>
+        public int LengthPerch
+        {
+            get
+            {
+                return _lengthPerch;
+            }
+            set
+            {
+                if (value <= 35 && value >= 25)
+                    _lengthPerch = value;
+                else
+                    throw new ArgumentException("Length perch has to be more 25 mm and less than 35 mm.");
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт диаметр жердочки.
+        /// </summary>
+        public int DiameterPerch
+        {
+            get
+            {
+                return _diameterPerch;
+            }
+            set
+            {
+                if (value <= 10 && value >= 5)
+                    _diameterPerch = value;
+                else
+                    throw new ArgumentException("Diameter perch has to be more 5 mm and less 10 mm." );
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт глубину скворечника.
+        /// </summary>
+        public int Depth
+        {
+            get
+            {
+                return _depth;
+            }
+            set
+            {
+                if (value <= 190 && value >= 120)
+                    _depth = value;
+                else
+                    throw new ArgumentException("Depth has to be more 120 mm and less 190 mm");
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт ширину скворечника.
+        /// </summary>
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if (value <= 190 && value >= 120)
+                    _width = value;
+                else
+                    throw new ArgumentException("Width has to be more 120 mm and less 190 mm.");
+            }
+        }
+        
+        /// <summary>
+        /// Возвращает и задаёт ширину крепежа скворечника.
+        /// </summary>
+        public int WidthFasteners
+        {
+            get
+            {
+                return _widthFasteners;
+            }
+            set
+            {
+                if (value <= 50 && value >= 30)
+                    _widthFasteners = value;
+                else
+                    throw new ArgumentException("Width fasteners has to be more 30 mm and less 50 mm.");
+            }
+        }
+    }
+
+}
